5873361a0f834e7c70141e70fe689ca89c43328a
Will Corcoran <corcoranwill@gmail.com>
2020-02-02 02:12:27 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,17 @@
 # Write your #display_rainbow method here
+
+
+# write a function called display_rainbow
+# method should receive an array of colors as an argument [red, orange, yellow, green, blue, indigo, violet]
+# print the colors in the following format: R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet
+# |--> select the active color and grab the first letter and make it uppercase
+
+
+
+
+def display_rainbow(colors)
+  colors.each do |color|
+    puts color
+end
+
+display_rainbow(["red", "orange", "yellow", "green", "blue", "indigo", "violet"])
