1e23ebde470cdc11bdcac4272826947ba254e77e
Will Corcoran <corcoranwill@gmail.com>
2020-02-02 02:19:35 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -12,7 +12,7 @@ colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 def display_rainbow(colors)
   colors.each do |color|
     puts color
-    puts color.slice.upcase(0)
+    first_letter = color.slice(0)
   end
 end
 
