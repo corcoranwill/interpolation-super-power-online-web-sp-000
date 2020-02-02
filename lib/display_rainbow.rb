494432278243b4e96543b5af06b6a8bca2867afc
Will Corcoran <corcoranwill@gmail.com>
2020-02-02 02:52:53 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,12 +3,12 @@ colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 
 def display_rainbow(colors)
   print "R: #{colors[0]}, "
-  # print "${colors[1].slice[0].upcase}: colors[1], "
-  # print "${colors[2].slice[0].upcase}: colors[2], "
-  # print "${colors[3].slice[0].upcase}: colors[3], "
-  # print "${colors[4].slice[0].upcase}: colors[4], "
-  # print "${colors[5].slice[0].upcase}: colors[5], "
-  # print "${colors[6].slice[0].upcase}: colors[6] "
+  print "O: #{colors[1]}, "
+  print "Y: #{colors[2]}, "
+  print "G: #{colors[3]}, "
+  print "B: #{colors[4]}, "
+  print "I: #{colors[5]}, "
+  print "V: #{colors[6]} "
 
   # colors.each do |color|
   #   first_letter = color.slice(0)
