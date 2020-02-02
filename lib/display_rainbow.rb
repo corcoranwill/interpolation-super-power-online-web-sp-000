36482632ff7475216cd9a81b0600201afbfdc50c
Will Corcoran <corcoranwill@gmail.com>
2020-02-02 02:39:43 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,7 +4,7 @@ colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 def display_rainbow(colors)
   colors.each do |color|
     first_letter = color.slice(0)
-    puts first_letter
+    puts first_letter.upcase
     # print "#{first_letter.upcase}: #{color}, "
   end
 end
